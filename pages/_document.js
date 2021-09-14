24db4f32c99e04d264bc86472a2777c9c89c877c
Kayron <kayron.scabral@gmail.com>
2021-09-14 17:42:52 -0300

    🚚 Rename title and description

--- pages/_document.js
+++ pages/_document.js
@@ -12,19 +12,16 @@ export default class MyDocument extends Document {
       <Html lang='pt-BR'>
         <Head>
           <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
-          <meta name='title' content='Focusely' />
-          <meta
-            name='description'
-            content='Focusely é um app fácil para usar a Técnica Pomodoro. Funciona em desktop e celular'
-          />
+          <meta name='title' content='Create Next App' />
+          <meta name='description' content='Create Next App' />
           <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
           <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
           <link rel='manifest' href='/manifest.json' />
           <link rel='mask-icon' href='/safari-pinned-tab.svg' color={theme.palette.primary.light} />
           <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
-          <meta name='apple-mobile-web-app-title' content='Focusely' />
-          <meta name='application-name' content='Focusely' />
+          <meta name='apple-mobile-web-app-title' content='Create Next App' />
+          <meta name='application-name' content='Create Next App' />
           <meta name='msapplication-TileColor' content='#f25c54' />
           <meta name='theme-color' content={theme.palette.primary.light} />
           <style>
