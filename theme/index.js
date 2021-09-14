e97b9f5ec7ed3a0ba1fd5ea66aa4df859980fc6e
Kayron <kayron.scabral@gmail.com>
2021-09-14 17:42:27 -0300

    💄 Remove custom theme

--- theme/index.js
+++ theme/index.js
@@ -1,18 +1,5 @@
 import { createTheme } from '@material-ui/core/styles'
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: '#F27059',
-      main: '#F25C54',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#59d6f2',
-      main: '#54c0f2',
-      contrastText: '#fff'
-    }
-  }
-})
+const theme = createTheme()
 
 export default theme
