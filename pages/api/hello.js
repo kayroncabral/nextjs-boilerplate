c298196c717efb7e24d200b822610558088dbd2b
Kayron <kayron.scabral@gmail.com>
2021-09-15 11:51:25 -0300

    ✨ Health check

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import connect from 'lib/mongodb'
-
-export default async function handler(req, res) {
-  const database = await connect()
-  console.log(database)
-
-  res.status(200).json({ name: 'John Doe' })
-}
