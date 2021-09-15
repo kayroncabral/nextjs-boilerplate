73b6902969057fcdb797b81b5d924cee1a6c1402
Kayron <kayron.scabral@gmail.com>
2021-09-15 09:55:05 -0300

    ✨ Mongodb integration

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connect from 'lib/mongodb'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const database = await connect()
+  console.log(database)
+
   res.status(200).json({ name: 'John Doe' })
 }
