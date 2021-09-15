5b1b990a5082c3f7283efae210923fe715f7094a
Kayron <kayron.scabral@gmail.com>
2021-09-15 09:54:43 -0300

    🐛 Fix metatag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Head from 'next/head'
+
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 
@@ -16,9 +18,12 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+      </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-          <Component {...pageProps} />
+        <Component {...pageProps} />
       </ThemeProvider>
     </>
   )
